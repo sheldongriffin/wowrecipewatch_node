682a1ee0dec0c28056f705154d96e9dbcbf672aa
Sheldon Griffin <chrisdavidgriffin@yahoo.com>
2015-05-11 15:35:35 -0500

    Add basic layout for items

--- server.js
+++ server.js
@@ -5,6 +5,43 @@ var app = express();
 
 var classes = { 1: "Warrior", 2:"Paladin", 3:"Hunter", 4:"Rogue", 5:"Priest", 6:"Death Knight", 7:"Shaman", 8:"Mage", 9:"Warlock", 10:"Monk", 11:"Druid" };
 var races = { 1: 'Human', 2: 'Orc', 3: 'Dwarf', 4: 'Night Elf', 5: 'Undead', 6: 'Tauren', 7: 'Gnome', 8: 'Troll', 9: 'Goblin', 10: 'Blood Elf', 11: 'Draenei', 22: 'Worgen', 25: 'Alliance Pandaren', 26: 'Horde Pandaren' };
+var slots = [
+	{ id: 1, name: 'head', pos: [0, -1, -1, 0] },
+	{ id: 2, name: 'neck', pos: [58, -1, -1, 0] },
+	{ id: 3, name: 'shoulder', pos: [116, -1, -1, 0] },
+	{ id: 4, name: 'back', pos: [174, -1, -1, 0] },
+	{ id: 5, name: 'chest', pos: [232, -1, -1, 0] },
+	{ id: 6, name: 'shirt', pos: [290, -1, -1, 0]},
+	{ id: 7, name: 'tabard', pos: [348, -1, -1, 0] },
+	{ id: 8, name: 'wrists', pos: [406, -1, -1, 0]},
+	{ id: 9, name: 'hands', pos: [0, 0, -1, -1] },
+	{ id: 10, name: 'waist', pos: [58, 0, -1, -1] },
+	{ id: 11, name: 'legs', pos: [116, 0, -1, -1] },
+	{ id: 12, name: 'feet', pos: [174, 0, -1, -1] },
+	{ id: 13, name: 'finger1', pos: [232, 0, -1, -1] },
+	{ id: 14, name: 'finger2', pos: [290, 0, -1, -1] },
+	{ id: 15, name: 'trinket1', pos: [348, 0, -1, -1] },
+	{ id: 16, name: 'trinket2', pos: [406, 0, -1, -1] },
+	{ id: 17, name: 'mainHand', pos: [-1, -1, 0, 273.5] },
+	{ id: 18, name: 'offHand', pos: [-1, -1, 0, 338.5] }
+];
+
+app.locals.positionHelper = function(pos){
+	var returnstr = '';
+	if (pos[0] != -1){
+		returnstr += 'top: ' + pos[0] + 'px;';
+	}
+	if (pos[1] != -1){
+		returnstr += 'right: ' + pos[1] + 'px;';
+	}
+	if (pos[2] != -1){
+		returnstr += 'bottom: ' + pos[2] + 'px;';
+	}
+	if (pos[3] != -1){
+		returnstr += 'left: ' + pos[3] + 'px;';
+	}
+	return returnstr;
+}
 
 app.set('view engine', 'ejs');
 
@@ -37,7 +74,7 @@ app.get('/character/:server/:charactername/simple', function (req, res, next){
 	fs.readFile(path.dirname(require.main.filename) + '/baseload/Doraie.json', { encoding: 'utf8' }, function (err, data) {
 		if (err) throw err;
 		var character = JSON.parse(data);
-		var view = { character: character, classes: classes, races: races };
+		var view = { character: character, classes: classes, races: races, slots: slots };
 		res.render('pages/character/simple', view);
 	});
 });
